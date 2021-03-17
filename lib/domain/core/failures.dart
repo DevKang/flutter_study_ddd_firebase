77e9e6c3577f14bc50b76c65c03efb5c37375e2d
Kang Hee Jong <olddeapo@gmail.com>
2021-03-17 22:07:32 +0900

    [2021-03-17] Lecture 11 Completed

--- lib/domain/core/failures.dart
+++ lib/domain/core/failures.dart
@@ -4,11 +4,28 @@ import 'package:flutter/foundation.dart';
 part 'failures.freezed.dart';
 
 @freezed
-abstract class ValueFailure<T> with _$ValueFailure<T> {
+class ValueFailure<T> with _$ValueFailure<T> {
+  // Notes Failures
+  const factory ValueFailure.exceedingLength({
+    required T failedValue,
+    required int max,
+  }) = ExceedingLength<T>;
+  const factory ValueFailure.empty({
+    required T failedValue,
+  }) = Empty<T>;
+  const factory ValueFailure.multiline({
+    required T failedValue,
+  }) = Multiline<T>;
+  const factory ValueFailure.listTooLong({
+    required T failedValue,
+    required int max,
+  }) = ListTooLong<T>;
+
+  // SignIn Failures
   const factory ValueFailure.invalidEmail({
-    required String failedValue,
+    required T failedValue,
   }) = InvalidEmail<T>;
   const factory ValueFailure.shortPassword({
-    required String failedValue,
+    required T failedValue,
   }) = ShortPassword<T>;
 }
